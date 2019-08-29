2d388eb7f6f6db6c92f4603e672d3c9b65af24da
kristie <kristiemconnor@gmail.com>
2019-08-29 01:28:57 -0400

    adds seed

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+actor.save
+
+actor = Actor.new({first_name: "Emily", last_name: "VanCamp", known_for: "Revenge"})
+actor.save
+
+actor = Actor.new({first_name: "Keanu", last_name: "Reeves", known_for: "The Matrix"})
+actor.save
+
+actor = Actor.new({first_name: "Allison", last_name: "Janney", known_for: "The West Wing"})
+actor.save
+
+actor = Actor.new({first_name: "Michael J.", last_name: "Fox", known_for: "Back To The Future"})
+actor.save
+
+actor = Actor.new({first_name: "Margot", last_name: "Robbie", known_for: "I, Tonya"})
+actor. save
