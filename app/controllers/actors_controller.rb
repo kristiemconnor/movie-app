4c86e069b10f7c23c2286ee3b3860aa3446d19af
kristie <kristiemconnor@gmail.com>
2019-09-09 11:25:05 -0400

    adds actors' ages

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -8,7 +8,8 @@ end
 def create
   @actor = Actor.new( first_name: params[:first_name],
     last_name: params[:last_name],
-    known_for: params[:known_for]
+    known_for: params[:known_for],
+      age: params[:age]
     )
   @actor.save
 
@@ -32,7 +33,8 @@ def update
 
   @actor.first_name = params[:first_name] || @actor.first_name
   @actor.last_name params[:last_name] || @actor.last_name
-  @actor.known_for = params[:known_for] || @actor.known_for
+  @actor.known_for = params[:known_for] || @actor.known_for,
+  @actor.age = params[:age] || @actor.age
   @actor.save
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock",
+  age: "43"})
 actor.save
 
-actor = Actor.new({first_name: "Emily", last_name: "VanCamp", known_for: "Revenge"})
+actor = Actor.new({first_name: "Emily", last_name: "VanCamp", known_for: "Revenge",
+  age: "33"})
 actor.save
 
-actor = Actor.new({first_name: "Keanu", last_name: "Reeves", known_for: "The Matrix"})
+actor = Actor.new({first_name: "Keanu", last_name: "Reeves", known_for: "The Matrix",
+  age: "55"})
 actor.save
 
-actor = Actor.new({first_name: "Allison", last_name: "Janney", known_for: "The West Wing"})
+actor = Actor.new({first_name: "Allison", last_name: "Janney", known_for: "The West Wing", age: "59"
+  })
 actor.save
 
-actor = Actor.new({first_name: "Michael J.", last_name: "Fox", known_for: "Back To The Future"})
+actor = Actor.new({first_name: "Michael J.", last_name: "Fox", known_for: "Back To The Future",
+ age: "58"})
 actor.save
 
-actor = Actor.new({first_name: "Margot", last_name: "Robbie", known_for: "I, Tonya"})
+actor = Actor.new({first_name: "Margot", last_name: "Robbie", known_for: "I, Tonya",
+  age: "29"})
 actor. save
 
 
